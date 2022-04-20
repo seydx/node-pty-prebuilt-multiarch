e3817fb77941eec6d90c2b6c6746ecc10f99bfe5
oznu <dev@oz.nu>
2022-04-20 11:56:04 +1000

    update docker build

--- build.sh
+++ build.sh
@@ -26,19 +26,19 @@ export DOCKERFILE="Dockerfile.debian"
 docker build -f .prebuild/$DOCKERFILE --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg QEMU_ARCH=${QEMU_ARCH} -t multiarch-build .
 docker run --rm -v $(pwd):/node-pty multiarch-build
 
-export BASE_IMAGE=library/node:15-alpine
+export BASE_IMAGE=library/node:16-alpine
 export QEMU_ARCH=x86_64
 export DOCKERFILE="Dockerfile.alpine"
 docker build -f .prebuild/$DOCKERFILE --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg QEMU_ARCH=${QEMU_ARCH} -t multiarch-build .
 docker run --rm -v $(pwd):/node-pty multiarch-build
 
-export BASE_IMAGE=arm32v6/node:15-alpine
+export BASE_IMAGE=arm32v6/node:16-alpine
 export QEMU_ARCH=arm
 export DOCKERFILE="Dockerfile.alpine"
 docker build -f .prebuild/$DOCKERFILE --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg QEMU_ARCH=${QEMU_ARCH} -t multiarch-build .
 docker run --rm -v $(pwd):/node-pty multiarch-build
 
-export BASE_IMAGE=arm64v8/node:15-alpine
+export BASE_IMAGE=arm64v8/node:16-alpine
 export QEMU_ARCH=aarch64
 export DOCKERFILE="Dockerfile.alpine"
 docker build -f .prebuild/$DOCKERFILE --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg QEMU_ARCH=${QEMU_ARCH} -t multiarch-build .
