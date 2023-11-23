f5d963c3caa3c4f1fa030fde306b4374dbc9c029
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2023-11-23 09:55:33 -0500

    Update build.sh

--- .prebuild/build.sh
+++ .prebuild/build.sh
@@ -4,13 +4,14 @@
 
 cd /node-pty
 # rm -rf node_modules
-
+echo
 echo "Building for $(uname -m)... ->" $*
 
 echo
-npm config set unsafe-perm true
+#npm config set unsafe-perm true
 #npm cache clean
-npm ci --ignore-scripts --no-bin-links
+#npm ci --ignore-scripts --no-bin-links
+npm ci --ignore-scripts
 # https://github.com/microsoft/vscode/blob/c23f0305dbf82b2319b198f4dbf3c5d5bc522f15/build/azure-pipelines/linux/product-build-linux-client.yml#L113-L125
 
 echo
@@ -21,7 +22,7 @@ echo
 
 #node .prebuild/build.js
 env JOBS=max node $*
-
+echo
 #env JOBS=max node .prebuild/prebuild.js -t 19.0.0 -t 20.0.0 -t 21.0.0
 #env JOBS=max node .prebuild/prebuildify.js -t 19.0.0 -t 20.0.0 -t 21.0.0
 
