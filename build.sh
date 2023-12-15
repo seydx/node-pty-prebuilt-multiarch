8ffe4bca51ee14df0738066944a2648421917c18
seydx <34152761+seydx@users.noreply.github.com>
2023-12-15 15:46:18 +0100

    add electron v28.0.0 (#36)

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export oldNodeBuildTargets='-t 10.0.0 -t 11.0.0 -t 12.0.0 -t 13.0.0 -t 14.0.0 -t
 export nodeBuildTargets='-t 19.0.0 -t 20.0.0 -t 21.0.0'
 
 export oldElectronBuildTargets='-t 5.0.0 -t 6.0.0 -t 7.0.0 -t 8.0.0 -t 9.0.0 -t 10.0.0 -t 11.0.0 -t 12.0.0 -t 13.0.0 -t 14.0.0 -t 15.0.0 -t 16.0.0 -t 17.0.0 -t 18.0.0 -t 19.0.0'
-export electronBuildTargets='-t 20.0.0 -t 21.0.0 -t 22.0.0 -t 23.0.0 -t 24.0.0 -t 25.0.0 -t 26.0.0 -t 27.0.0'
+export electronBuildTargets='-t 20.0.0 -t 21.0.0 -t 22.0.0 -t 23.0.0 -t 24.0.0 -t 25.0.0 -t 26.0.0 -t 27.0.0 -t 28.0.0'
 
 export oldRunCMD="./.prebuild/build.sh .prebuild/prebuild.js ${oldNodeBuildTargets} && \
 ./.prebuild/build.sh .prebuild/prebuildify.js ${oldNodeBuildTargets} && \
